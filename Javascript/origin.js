81e6b4fb29f41510c34a51d002c1ec4a2fa25dbf
Jeriah Neal <Natalect.org@protonmail.com>
2023-03-20 15:44:05 -0700

    Added console.logs for testing if opened on main github page

--- Javascript/origin.js
+++ Javascript/origin.js
@@ -8,7 +8,9 @@ if (origin === "https://monohen.github.io") {
     .setAttribute("content", "10; " + originURL);
   document.getElementById("websiteName").setAttribute("href", originURL);
   document.getElementById("websiteName").lastChild.innerText = "Monohen";
+  console.log("opened from github monohen.github.io");
 } else {
+  console.log("opened from local");
 }
 
 loadFeaturedGames();
